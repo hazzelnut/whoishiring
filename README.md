87e6f93f6aae5b83002ab79cad30c98da1184241
Eric Chan <hazzelnut@pm.me>
2023-04-16 15:13:35 -0400

    basic app running

---
+++ README.md
@@ -0,0 +1,23 @@
+## Whoishiring Job board
+
+I'm using SvelteKit to make this app. Instructions below!
+
+## Developing
+```bash
+npm run dev
+
+# or start the server and open the app in a new browser tab
+npm run dev
+```
+
+## Building
+
+To create a production version of the app:
+
+```bash
+npm run build
+```
+
+You can preview the production build with `npm run preview`.
+
+> To deploy your app, you may need to install an [adapter](https://kit.svelte.dev/docs/adapters) for your target environment.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import type { PageData } from './$types';
+
+  export let data: PageData;
+
+  let sorting = "newest";
+</script>
+
+<style>
+  div > p {
+    color: green;
+  }
+</style>
+
+<button>sort by {sorting}</button>
+
+{#each data.posts as post}
+  {#if post?.deleted == true}
+    <p>deleted!</p> 
+  {/if}
+  <div>
+    <p>{post.by}</p>
+    <p>{post.timeAgo}</p>
+    {@html post.text}
+    <!-- {console.log(post)} -->
+  </div>
+
+  <!-- There's a progress bar for how far I'm scrolling. ie how many job posts left to read -->
+  <!-- Seen and unseen feature. Can filter by posts I've seen -->
+  <br />
+{/each}
