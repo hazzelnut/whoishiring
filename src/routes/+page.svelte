ed17bb2ad6a2b325abf7ee5b3038bc7b032c5a2d
Eric Chan <hazzelnut@pm.me>
2023-04-17 19:13:22 -0400

    basiccc styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,46 +1,105 @@
 <script lang="ts">
-	import { invalidate, invalidateAll } from '$app/navigation';
+	import { invalidate } from '$app/navigation';
   import type { PageData } from './$types';
 
   export let data: PageData;
 
+  /* Sorting posts */
   let sort = 'newest';
-
-  function toggleSort () {
+  function toggleSort() {
     sort = sort === 'oldest' ? 'newest' : 'oldest'
-    // Trigger a load server-side?
+    // Note: trigger a re-load of the load function
     invalidate(url => url.searchParams.has('sort'));
+  }
+
+  /* Saved posts */
+  let savedPosts: Record<number, string>= {};
+  function handleSave(id: number, text: string) {
+    if (Object.hasOwn(savedPosts, id)) {
+      const { [id]: _deletedPost, ...restPosts} = savedPosts
+      savedPosts = restPosts;
+    } else {
+      savedPosts = {...savedPosts, [id]: text}
+    }
   }
 
 </script>
 
 <style>
-  div > p {
-    color: green;
+  main {
+    padding: 1em;
+  }
+  p {
+    word-wrap: break-word;
+  }
+  button {
+    height: 4em;
+    width: 8em;
+  }
+
+  div.savedStats {
+    margin: 1em 0;
+  }
+  div.post {
+    position: relative;
+    margin: 1em 0;
+    padding: 1em;
+    border: 1px solid black;
+    border-radius: 1em;
+  }
+  div.postInfo{
+    font-weight: bold;
+  }
+  p.timeAgo {
+    color: rgb(77, 163, 183);
+  }
+
+  button.saveBtn {
+    position: absolute;
+    top: 1em;
+    right: 1em;
+  }
+  button.saved {
+    font-weight: bolder;
+    color: rgb(90, 188, 106);
   }
 </style>
 
-<form>
-  <button
-    name="sort"
-    value={sort}
-    on:click={toggleSort}
-  >{sort}</button>
-</form>
+<main>
+  <form>
+    <button
+      name="sort"
+      value={sort}
+      on:click={toggleSort}
+    >{sort}</button>
+  </form>
 
-{#each data.posts as post}
-  {#if post?.deleted == true}
-    <p>deleted!</p> 
-  {/if}
-  <div>
-    <p>{post.by}</p>
-    <p>{post.timeAgo}</p>
-    <span>{@html post.text}</span>
-    <!-- {console.log(post)} -->
+  <div class="savedStats">
+    Saved Posts: {Object.keys(savedPosts).length}
   </div>
-  <!-- Goal: To have the url be shareable. Need to modify URL with each new filter -->
 
-  <!-- There's a progress bar for how far I'm scrolling. ie how many job posts left to read -->
-  <!-- Seen and unseen feature. Can filter by posts I've seen -->
-  <br />
-{/each}
+  {#each data.posts as post}
+    <div class="post">
+      <div class="postInfo">
+        <p>{post.by}</p>
+        <p class="timeAgo">{post.timeAgo}</p>
+      </div>
+      <p>{@html post.text}</p>
+      <button
+        class={
+          'saveBtn ' + 
+          `${Object.hasOwn(savedPosts, post.id) && "saved"}`
+        }
+        on:click={() => handleSave(post.id, post.text)}
+      >
+        {Object.hasOwn(savedPosts, post.id) ? 'SAVED' : 'Save'}
+      </button>
+      <!-- {console.log(post)} -->
+    </div>
+    <!-- Goal: To have the url be shareable. Need to modify URL with each new filter -->
+
+    <!-- There's a progress bar for how far I'm scrolling. ie how many job posts left to read -->
+    <!-- Seen and unseen feature. Can filter by posts I've seen -->
+    <br />
+  {/each}
+</main>
