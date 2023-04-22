ca303fcba56e583442dfbaf1e5d551458f4bc6f2
Eric Chan <hazzelnut@pm.me>
2023-04-21 22:56:08 -0400

    add supabase api for faster loads

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,35 +1,77 @@
+import { createClient } from '@supabase/supabase-js';
 import { getLatestStoryAndItems, type ItemJson } from '../lib/fetch';
 import { getTimeAgo } from '../lib/normalize';
 import type { PageServerLoad } from './$types';
+import { env } from '$env/dynamic/private';
 
 export interface ExtendedItemJson extends ItemJson {
   timeAgo: string;
 } 
 
-export const prerender = true;
+async function getJobs(): Promise<ExtendedItemJson[]> {
+  const supabaseUrl = 'https://unlkhbznammyxxtrejhq.supabase.co'
+  const supabaseKey = env.SUPABASE_KEY;
+  const supabase = createClient(supabaseUrl, supabaseKey);
+  const response = await supabase.from("jobs").select();
+  // NOTE: Can't guarantee type here if we edit DB
+  //       would prisma solve this?
+  return response.data as ExtendedItemJson[];
+}
 
-export const load = (async ({ fetch, url }) => {
-  const sort = url.searchParams.get('sort');
-  // NOTE: Prevents browser warnings from Svelte for using
-  //       browser fetch on external URLs.
+// TODO: Write serverless function to run
+// cron job to fill data into supabase database
+// to add complete data
+
+/*  Serverless function to cron to insert into Supabase */
+async function addJobsToSupabase(posts: ExtendedItemJson[]) {
+  const supabaseUrl = 'https://unlkhbznammyxxtrejhq.supabase.co'
+  const supabaseKey = env.SUPABASE_KEY;
+  const supabase = createClient(supabaseUrl, supabaseKey);
+  posts.map(async (post) => {
+    try {
+      const response = await supabase.from("jobs").insert({
+        ...post,
+        kids: JSON.stringify(post?.kids || []),
+        json: JSON.stringify(post)
+      });
+      console.log(response);
+    } catch(e) {
+      console.error(e)
+    }
+  });
+}
+
+async function getJobsFromHN(): Promise<ExtendedItemJson[]> {
   // Ref:  https://kit.svelte.dev/docs/load#making-fetch-requests
   let posts = await getLatestStoryAndItems(fetch);
 
-  // Sort by latest to oldest
+  // filter dead and deleted posts
+  posts = posts.filter(post => !post?.dead && !post?.deleted);
+
+  // Add a few nice-to-have params to display on front-end
+  // but insert into Supabase DB
+  const updatedPosts = posts.map(post => ({
+    ...post,
+    timeAgo: getTimeAgo(post.time)}
+  )) as ExtendedItemJson[];
+
+  return updatedPosts;
+}
+
+
+
+export const load = (async ({ url }) => {
+  const sort = url.searchParams.get('sort');
+  const posts = await getJobs();
+
+  // Sort by latest to oldest;
+  // Faster to sort client side than in a order query
+  // to Supabase
   if (sort == 'newest') {
     posts.sort((postA, postB) => postB.time - postA.time);
   } else {
     posts.sort((postA, postB) => postA.time - postB.time);
   }
 
-  // filter dead and deleted posts
-  posts = posts.filter(post => !post?.dead && !post?.deleted);
-
-  // Add a few nice-to-have params to display on front-end
-  const updatedPosts = posts.map(post => ({
-    ...post,
-    timeAgo: getTimeAgo(post.time)}
-  )) as ExtendedItemJson[];
-
-  return { posts: updatedPosts };
+  return { posts };
 }) satisfies PageServerLoad;
