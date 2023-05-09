b81773810807387f6cb08bf47727561dc9dd7e2d
Eric Chan <hazzelnut@pm.me>
2023-05-08 23:30:45 -0400

    temp playground for loading and fetching data from supabase

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,14 +1,31 @@
-import { getJobs } from '$lib/fetch';
+import { getJobs, getLatestStoryHN, upsertItems, upsertStory } from '$lib/fetch';
 import type { PageServerLoad } from './$types';
-import { tags as popularTags } from '$lib/tags';
 
 
 export const load = (async ({ url }) => {
   // const postToLoad = await getJobsFromHN()
   // await addJobsToSupabase(postToLoad)
 
-  const {data: posts, count: totalCount } = await getJobs(url);
+  // const {data: posts, count: totalCount } = await getJobs(url);
   // TODO: Need to return popular tags based on data ingestion time
+  const latestStoryHN = await getLatestStoryHN()
+  const latestStory = await upsertStory(latestStoryHN)
+  if (!latestStory) throw new Error('Latest story upserted could not be returned!')
 
-  return { posts, startIndex: 0, totalCount, popularTags };
+  const items = await upsertItems(latestStoryHN.kids, latestStory.id)
+  if (!items) throw new Error('Items userted could not be returned!')
+
+  // Keep a record of the counts for each tag in all the items 
+  const tagsToCounts = items.reduce((counts: Record<string, number>, item) => {
+    if (item && item?.tags)
+      item.tags.forEach(tag => {
+        counts[tag] = (counts[tag] || 0) + 1
+      })
+    return counts
+  }, {});
+
+  console.log('Record of tag popularity: ', tagsToCounts)
+  // Need to sort by count and popularity?
+
+  // return { posts, startIndex: 0, totalCount, popularTags };
 }) satisfies PageServerLoad;
