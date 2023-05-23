4a7b3eabd26ae790dd80e77f265c24e17db8123b
Eric Chan <hazzelnut@pm.me>
2023-05-23 16:28:11 -0400

    remove serverless ingestion functions

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -1,9 +1,7 @@
 import pLimit from "p-limit";
 import supabase from "./supabase/client";
-import { tags } from "./tags";
-import { htmlToText } from "html-to-text";
 
-export interface ItemJson {
+interface ItemJson {
   by: string;
   id: number;
   parent: number;
@@ -13,10 +11,6 @@ export interface ItemJson {
   deleted?: boolean;
   dead?: boolean;
   kids?: number[];
-}
-
-export interface ExtendedItemJson extends ItemJson {
-  timeAgo: string;
 }
 
 interface StoryItemJson {
@@ -61,7 +55,7 @@ export async function getLatestStoryAndItems(): Promise<ItemJson[]> {
   // okay with Vercel?
   const limit = pLimit(20);
   const jobPosts: Array<ItemJson> = await Promise.all(
-    itemIds.map(async (itemId: number) => {
+    itemIds.map((itemId: number) => {
       return limit(async () => {
         return await fetchItemJson<ItemJson>(itemId);
       })
@@ -133,192 +127,6 @@ export async function getJobs(url: URL) {
   }
 }
 
-/****** Serverless code for data ingestion *******/
-// TODO: Write serverless function to run
-// cron job to fill data into supabase database
-// to add complete data
-
-// /*  Serverless function to cron to insert into Supabase */
-// export async function addJobsToSupabase(posts: ExtendedItemJson[]) {
-//   posts.map(async (post) => {
-//     try {
-//       const response = await supabase.from("jobs").insert({
-//         ...post,
-//         kids: JSON.stringify(post?.kids || []),
-//         json: JSON.stringify(post)
-//       });
-//       console.log(response);
-//     } catch(e) {
-//       console.error(e)
-//     }
-//   });
-// }
-
-// export async function getJobsFromHN(): Promise<ExtendedItemJson[]> {
-//   // Ref:  https://kit.svelte.dev/docs/load#making-fetch-requests
-//   let posts = await getLatestStoryAndItems();
-
-//   // filter dead and deleted posts
-//   posts = posts.filter(post => !post?.dead && !post?.deleted);
-
-//   // Add a few nice-to-have params to display on front-end
-//   // but insert into Supabase DB
-//   const updatedPosts = posts.map(post => ({
-//     ...post,
-//     timeAgo: getTimeAgo(post.time)}
-//   )) as ExtendedItemJson[];
-
-//   return updatedPosts;
-// }
-
-//  Function to fetch whoishiring post and comments
-export async function fetchItemJson<T>(itemId: number): Promise<T> {
-  const firebaseUrl = "https://hacker-news.firebaseio.com/v0";
-  const firebaseItem = (itemId: number) => `${firebaseUrl}/item/${itemId}.json`;
-
-  const url = firebaseItem(itemId);
-  const resp = await fetch(url);
-  const json = (await resp.json()) as T;
-  return json;
-};
-
-
-export async function getLatestStoryHN(): Promise<StoryItemJson> {
-  const firebaseUrl = "https://hacker-news.firebaseio.com/v0";
-  const firebaseStories = await fetch(
-    `${firebaseUrl}/user/whoishiring/submitted.json`
-  );
-
-  const stories = (await firebaseStories.json()) as Array<number>;
-
-  // The first story from whoishiring user
-  // is always the latest 'Ask HN: Who is Hiring?'
-  const storyId = stories[0];
-  const latest = await fetchItemJson<StoryItemJson>(storyId);
-
-  if (!latest?.title?.match(/Ask HN: Who is hiring/)) {
-    throw new Error("Story is not 'who is hiring'");
-  }
-
-  return latest
-}
-
-export async function upsertStory(storyHN: StoryItemJson) {
-  const { id: firebaseId, title, time } = storyHN
-  const upsert = {
-    updatedAt: new Date().toISOString(),
-    firebaseCreatedAt: new Date(time * 1000).toISOString(),
-    firebaseId,
-    title,
-  }
-
-  // Ref: https://supabase.com/docs/reference/javascript/upsert
-  // ignoreDuplicates: false -> 'duplicate rows merged with existing rows'
-  // Create or update existing row
-  const { data } = await supabase.from('Story')
-                                 .upsert({ ...upsert}, { onConflict: 'firebaseId', ignoreDuplicates: false }) 
-                                 .select()
-                                 .limit(1)
-                                 .single()
-
-  if (!data) throw new Error('Latest story upserted could not be returned!')
-  return data
-}
-
-export function matchTags(text: string) {
-  return tags.reduce((accum: string[], slug) => {
-    const escaped = slug.replace(/(\.|\+)/g, `\\$&`);
-
-    // My version of the proximity '<->' Postgres full-text search.
-    // Match each word followed immediately by the next word.
-    const zeroProximity = escaped.split(' ').join(`(.?|)`)
-
-    // Match the word separately, by itself
-    const boundary = `\\b${zeroProximity}\\b`
-    const re = new RegExp(boundary, "gi");
-
-    if (text.match(re)) accum.push(slug);
-    return accum;
-  }, []);
-};
-
-interface ItemTextMatches {
-  remote: boolean;
-  tags: string[];
-}
-export async function scanItemText(text: string): Promise<ItemTextMatches> {
-  const matcher = (m: string): boolean => !!text.match(new RegExp(m, "gi"));
-
-  const remote = matcher("remote");
-  const tags = await matchTags(text);
-
-  return { remote, tags };
-};
-
-export async function upsertItems(itemIds: number[], storyId: number) {
-  const limit = pLimit(20);
-
-  const itemsInserted = await Promise.all(
-    itemIds.map(async (itemId: number) => {
-      return limit(async () => {
-        const item = await fetchItemJson<ItemJson>(itemId);
-
-        if (!item || item?.deleted || item?.dead) {
-          console.log('--- skipping ---', item)
-          return
-        }
-
-        const { by, time, text: htmlText, id: firebaseId } = item
-        const text = htmlToText(htmlText)
-        const { remote, tags } = await scanItemText(text)
-
-        const upsert = {
-          by,
-          text,
-          htmlText,
-          remote,
-          firebaseId,
-          firebaseCreatedAt: new Date(time * 1000).toISOString(),
-          updatedAt: new Date().toISOString(),
-          json: JSON.stringify(item),
-          storyId,
-          tags,
-        }
-
-        // Ref: https://supabase.com/docs/reference/javascript/upsert
-        // ignoreDuplicates: false -> 'duplicate rows merged with existing rows'
-        // Create or update existing row
-        const { data } = await supabase.from('Item')
-                                            .upsert({ ...upsert }, { onConflict: 'firebaseId', ignoreDuplicates: false}) // if text has changed, update the item
-                                            .select()
-                                            .limit(1)
-                                            .single()
-
-        return { ...data }
-      })
-    })
-  );
-
-  return itemsInserted
-}
-
-export async function upsertStoryToTags(tagsToCounts: Record<string, number>, storyId: number) {
-  for (const [tag, count] of Object.entries(tagsToCounts)) {
-    const storyToTagId = `${storyId}${tag}`
-    const upsert = {
-      storyId,
-      tag,
-      count,
-      storyToTagId,
-      updatedAt: new Date().toISOString(),
-    }
-    const response  = await supabase.from('StoryToTags')
-                                    .upsert({ ...upsert }, { onConflict: 'storyToTagId', ignoreDuplicates: false})
-                                    .select()
-
-    console.log('StoryToTags response: ', response)
-  }
-}
 
 // TODO: get the latestStory
 export async function getPopularTags(storyId: number) {
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,29 +1,8 @@
-import { getJobs, getLatestStory, getLatestStoryHN, getPopularTags, upsertItems, upsertStory, upsertStoryToTags } from '$lib/fetch';
+import { getJobs, getLatestStory } from '$lib/fetch';
 import type { PageServerLoad } from './$types';
 
 
 export const load = (async ({ url }) => {
-  // // /* Serverless data ingestion functions */
-  // const latestStoryHN = await getLatestStoryHN()
-  // const latestStory = await upsertStory(latestStoryHN)
-
-  // const latestItems = await upsertItems(latestStoryHN.kids, latestStory.id)
-  // if (!latestItems) throw new Error('Items userted could not be returned!')
-
-  // // Keep a record of the counts for each tag in all the items 
-  // const tagsToCounts = latestItems.reduce((counts: Record<string, number>, item) => {
-  //   if (item && item?.tags)
-  //     item.tags.forEach(tag => {
-  //       counts[tag] = (counts[tag] || 0) + 1
-  //     })
-  //   return counts
-  // }, {});
-
-
-  // // store this record in StoryToTags
-  // await upsertStoryToTags(tagsToCounts, latestStory.id)
-
-  // TODO: Load popular tags in the browser instead?
   let storyId = url.searchParams.get('storyId')
   if (!storyId) {
     const latestStory = await getLatestStory()
