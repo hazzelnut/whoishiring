490ae59ad652dc405591781d76e3f40c0ba672b1
Eric Chan <hazzelnut@pm.me>
2023-05-25 12:35:18 -0400

    update supabase project url to be env var

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -2,7 +2,7 @@ import { env } from "$env/dynamic/private";
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "./schema";
 
-const supabaseUrl = 'https://unlkhbznammyxxtrejhq.supabase.co'
+const supabaseUrl = env.SUPABASE_URL;
 const supabaseKey = env.SUPABASE_KEY;
 const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
