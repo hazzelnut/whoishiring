fef4b9e134376932960eb003d8e1e2795782cc63
Eric Chan <hazzelnut@pm.me>
2023-06-12 18:07:01 -0400

    update schema with capitalize vars

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,7 @@
 import { sql } from "drizzle-orm";
 import { serial, text, timestamp, integer, pgTable, jsonb, boolean, uniqueIndex } from "drizzle-orm/pg-core";
  
-export const story = pgTable("Story", {
+export const Story = pgTable("Story", {
   id: serial("id").primaryKey(),
   title: text("title").notNull(),
   firebaseId: integer("firebaseId").notNull(),
@@ -14,14 +14,14 @@ export const story = pgTable("Story", {
   }
 });
 
-export const item = pgTable("Item", {
+export const Item = pgTable("Item", {
   id: serial("id").primaryKey(),
   by: text("by").notNull(),
   text: text("text").notNull(),
   htmlText: text("htmlText").notNull(),
   firebaseId: integer("firebaseId").notNull(),
   firebaseCreatedAt: timestamp("firebaseCreatedAt", { precision: 3, withTimezone: true }).notNull(),
-  storyId: integer("storyId").notNull().references(() => story.id, { onUpdate: 'cascade', onDelete: 'cascade' }),
+  storyId: integer("storyId").notNull().references(() => Story.id, { onUpdate: 'cascade', onDelete: 'cascade' }),
   json: jsonb("json"),
   remote: boolean("remote").notNull().default(false),
   tags: text("tags").array().notNull(), 
@@ -35,9 +35,9 @@ export const item = pgTable("Item", {
   }
 });
 
-export const storyToTags = pgTable("StoryToTags", {
+export const StoryToTags = pgTable("StoryToTags", {
   id: serial("id").primaryKey(),
-  storyId: integer("storyId").notNull().references(() => story.id, { onUpdate: 'cascade', onDelete: 'cascade' }),
+  storyId: integer("storyId").notNull().references(() => Story.id, { onUpdate: 'cascade', onDelete: 'cascade' }),
   tag: text("tag").notNull(),
   count: integer("count").notNull(),
   storyToTagId: text("storyToTagId").notNull(),
@@ -49,8 +49,3 @@ export const storyToTags = pgTable("StoryToTags", {
     storyToTagIdIdx: uniqueIndex("StoryToTags_storyToTagId_key").on(table.storyToTagId),
   }
 });
-
-
-
-// TODO: run migrations -> drizzle && node migrate.js
-// TODO: get a local postgres db running, with DATABASE_URL exported
