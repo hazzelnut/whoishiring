58ff3f653ec6243795423b7b957683ca56c82c92
Eric Chan <hazzelnut@pm.me>
2023-06-12 18:06:33 -0400

    add scripts to ingest data

---
+++ scripts/migrate.ts
@@ -0,0 +1,15 @@
+import { drizzle, } from 'drizzle-orm/postgres-js';
+import { migrate } from 'drizzle-orm/postgres-js/migrator';
+import postgres from 'postgres';
+ 
+// for migrations
+console.log(process.env.DATABASE_URL)
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const migrationClient = postgres(process.env.DATABASE_URL!, { max: 1 });
+try {
+  await migrate(drizzle(migrationClient), { migrationsFolder: './drizzle' })
+} catch (e) {
+  throw new Error(e)
+}
+
+process.exit()
