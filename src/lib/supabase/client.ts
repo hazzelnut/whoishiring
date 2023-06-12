af9bbcaafc60798ac6cd73df0636c6876a7f290d
Eric Chan <hazzelnut@pm.me>
2023-06-12 18:06:11 -0400

    remove supabase files

--- src/lib/supabase/client.ts
+++
@@ -1,9 +0,0 @@
-import { env } from "$env/dynamic/private";
-import { createClient } from "@supabase/supabase-js";
-import type { Database } from "./schema";
-
-const supabaseUrl = env.SUPABASE_URL;
-const supabaseKey = env.SUPABASE_KEY;
-const supabase = createClient<Database>(supabaseUrl, supabaseKey);
-
-export default supabase
