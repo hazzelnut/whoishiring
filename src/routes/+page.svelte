709ea4b1f2a7a2aede4c664e1b29c5db95ffb383
Eric Chan <hazzelnut@pm.me>
2023-06-20 21:11:16 -0400

    update placeholder text and sort button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -136,7 +136,11 @@
   <!-- TODO: Do a manual form submit :/ -->
   <form data-sveltekit-noscroll>
     <div class="search-container">
-      <input type="text" bind:value={search} />
+      <input
+        type="text"
+        placeholder="Search..."
+        bind:value={search}
+      />
 
       <button type="submit" class="search-submit">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
@@ -153,8 +157,6 @@
       </button>
     </div>
 
-    <br />
-    <button on:click={() => toggleSort()}>{sort}</button>
 
     <br />
     {#if tags.length > 0}
@@ -169,6 +171,9 @@
 
     <!-- Buttons do a form submit without page refresh -->
     <button on:click={() => remote = !remote}>Remote Only: {remote}</button>
+
+    <br />
+    <button on:click={() => toggleSort()}>{sort}</button>
 
     <!-- Use hidden input to send sort filter to URL -->
     <input
